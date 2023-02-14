cf15b53ea3bd2d164699dcd3d1de6ffe0cf4d12c
mellambias <mellambias@gmail.com>
2023-02-14 19:57:43 +0100

    model

---
+++ public/dashboard/models/book.js
@@ -0,0 +1,59 @@
+import Model, { DataTypes } from '../dao/Model.js';
+
+// propiedades obligatorias
+// presence: true equivale a allowNull:false
+
+export default class Book extends Model {
+    constructor(connection) {
+        super(connection);
+    }
+
+    defineModel() {
+        return {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            title: {
+                type: DataTypes.STRING,
+                presence: true,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+            author: {
+                type: DataTypes.STRING,
+                presence: true,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+            description: {
+                type: DataTypes.TEXT,
+                presence: true,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+            isbn: {
+                type: DataTypes.STRING,
+                presence: true,
+                validate: {
+                    notEmpty: true,
+                    isbn: true,
+                },
+            },
+            pageCount: {
+                type: DataTypes.INTEGER,
+                presence: true,
+                validate: {
+                    isInt: true,
+                },
+            },
+            publishedDate: {
+                type: DataTypes.DATE,
+            },
+        };
+    }
+}
